ac5276dc60fd0cb336fa0f430f37ab72ce519a86
Pedro Serey <serey.pedro@gmail.com>
2023-01-14 14:07:19 -0300

    add: vagaService

--- src/vagaService.pl
+++ src/vagaService.pl
@@ -1,19 +1,13 @@
-:- module(teste, [
-    estacionaVeiculo/0,
-    pagaEstacionamento/0,
+:- module(vagaService, [
     vagasDisponiveis/0,
     vagasDisponiveisAndar/0,
-    tempoVaga/0,
     adicionaVaga/0,
     adicionaAndar/0,
     adicionaTempoVaga/0
     ]).
 
-estacionaVeiculo :- halt.
-pagaEstacionamento :- halt.
 vagasDisponiveis :- halt.
 vagasDisponiveisAndar :- halt.
-tempoVaga :- halt.
 adicionaVaga :- halt.
 adicionaAndar :- halt.
 adicionaTempoVaga :- halt.
