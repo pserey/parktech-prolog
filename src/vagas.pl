d7b2226ff93106c002543655805d7184a9a3671d
Pedro Serey <serey.pedro@gmail.com>
2023-01-21 18:12:33 -0300

    add: db de vagas

---
+++ src/vagas.pl
@@ -0,0 +1,5 @@
+vaga(0,3,3,carro,1674331260,3-carro-3,none).
+vaga(0,3,1,moto,1674331310,3-moto-1,none).
+vaga(0,3,6,van,1674331354,3-van-6,none).
+vaga(0,3,3,carro,1674334264,3-carro-3,none).
+vaga(0,8,3,van,1674334285,3-van-3,none).
