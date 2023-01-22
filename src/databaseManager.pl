6bfc8b6d563f0bf44054989d25b594f85b5e63a3
Pedro Serey <serey.pedro@gmail.com>
2023-01-22 18:45:36 -0300

    docs: documentação de input_line/1 e addFact/2

--- src/databaseManager.pl
+++ src/databaseManager.pl
@@ -1,6 +1,9 @@
 :- module(databaseManager, [
     addFact/2]).
 
+% addFact(+Arquivo, +Fact)
+% adiciona fato passado como parametro a arquivo
+% Arquivo deve ser string com path para arquivo (a partir do PWD)
 addFact(Arquivo, Fact) :-
     open(Arquivo, append, Stream),
     format(Stream, '~w.~n', [Fact]),
--- src/util.pl
+++ src/util.pl
@@ -1,4 +1,6 @@
 :- module(util, [input_line/1]).
 
+% input_line(-Line)
+% lê linha de input e retorna string
 input_line(Line) :-
     read_line_to_string(user_input, Line).
