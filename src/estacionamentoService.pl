a87ba86dddfeb0d3d2ffe78e7070e2ac88e450bd
jhenriwue <joao.ramalho@ccc.ufcg.edu.br>
2023-01-29 23:38:21 -0300

    almost complete taxa_pagamento function

--- src/estacionamentoService.pl
+++ src/estacionamentoService.pl
@@ -12,16 +12,21 @@ horas(FINAL, INICIAL, HORAS) :-
     round(INICIAL, DATE2),
     HORAS is DATE1 - DATE2.
 
-retorna_horas(TEMPO_VAGA, HORAS) :-
+retorna_horas(TEMPO_VAGA, Horas) :-
     TEMPO_VAGA = RETORNO,
-    HORAS is RETORNO / 3600.
+    Horas is RETORNO / 3600.
 
-taxa_pagamento(Vaga, ISDiaSemana, Taxa) :-
+taxa_pagamento(IdVaga, ISDiaSemana, Taxa) :-
+    vaga(IdVaga, NumNovo, Andar, TipoVeiculo, Now, IdVaga,_),
     posix_time(Current),
-    DataFinal is Current,
-    tempo_inicial(Vaga, Inicial),
-    DataInicial is Inicial,
-    horas(DataFinal, DataInicial, Diferenca),
-    D is Diferenca.
+    horas(Current, Now, Diferenca),
+    retorna_horas(Diferenca, Horas),
+    (TipoVeiculo == 'carro' -> Horas > 2 -> ISDiaSemana -> Taxa is (6 + (((Horas) -2) * 1.5)) ; Taxa is (8 + (((Horas) -2) * 2))),
+    (TipoVeiculo == 'carro' -> Horas < 2 -> ISDiaSemana -> Taxa is 6 ; Taxa is 8),
+    (TipoVeiculo == 'moto' -> Horas > 2 -> ISDiaSemana -> Taxa is (4 + (((Horas) -2))) ; Taxa is (6 + (((Horas) -2) * 1.5))),
+    (TipoVeiculo == 'moto' -> Horas < 2 -> ISDiaSemana -> Taxa is 4 ; Taxa is 6),
+    (TipoVeiculo == 'van' -> Horas > 2 -> ISDiaSemana -> Taxa is (8 + (((Horas) -2) * 2)) ; Taxa is (10 + (((Horas) -2) * 2.5))),
+    (TipoVeiculo == 'van' -> Horas < 2 -> ISDiaSemana -> Taxa is 8 ; Taxa is 10),
+
 paga_estacionamento :- nl, write('paga_estacionamento').
 tempo_vaga :- nl, write('tempo_vaga').
