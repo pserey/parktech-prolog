7d42dc4bb3d5e851eaae8c67a60cec0c386d0af2
Pedro Serey <serey.pedro@gmail.com>
2023-02-02 16:15:05 -0300

    add: verifica veiculo

--- src/veiculoService.pl
+++ src/veiculoService.pl
@@ -1,12 +1,21 @@
 :- module(veiculoService, [
-    cadastraVeiculo/1
+    cadastra_veiculo/1,
+    verifica_veiculo/1
     ]).
 :- use_module('util.pl', [input_line/1, posix_time/1]).
 :- use_module('databaseManager.pl', [add_fact/2]).
 :- use_module('menu.pl', [menu/0]).
+
+:- dynamic veiculo/3.
 
 cadastra_veiculo(placa) :- 
     write('Insira o tipo de veículo: '), input_line(tipo),
     write('Insira a cor do veículo: '), input_line(cor),
     add_fact('src/veiculo.pl', veiculo(tipo, placa, cor)),
     menu.
+
+verifica_veiculo(Placa) :-
+    consult('src/veiculos.pl'),
+    veiculo(_, PlacaCompound, _),
+    with_output_to(atom(Placa), write(PlacaCompound)).
+    % file_to_facts('src/clientes.pl', Clientes),
--- src/veiculos.pl
+++ src/veiculos.pl
@@ -1,8 +1,8 @@
-veiculo('carro', '1111cccc', 'branco').
-veiculo('carro', '2222cccc', 'preto').
-veiculo('carro', '3333cccc', 'azul').
-veiculo('moto', '1111mmmm', 'branco').
-veiculo('moto', '2222mmmm', 'preto').
-veiculo('moto', '3333mmmm', 'azul').
-veiculo('van', '1111vvvv', 'branco').
-veiculo('van', '1111vvvv', 'verde').
+veiculo(carro, 1111-cccc, branco).
+veiculo(carro, '2222-cccc', preto).
+veiculo(carro, '3333-cccc', azul).
+veiculo(moto, '1111-mmmm', branco).
+veiculo(moto, '2222-mmmm', preto).
+veiculo(moto, '3333-mmmm', azul).
+veiculo(van, '1111-vvvv', branco).
+veiculo(van, '1111-vvvv', verde).
