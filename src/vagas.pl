367de21a54495adc164f4891a1c0135790bfa202
wendell.nascimento <wendell.nascimento@ccc.ufcg.edu.br>
2023-02-03 11:48:16 -0300

    fix banco

--- src/vagas.pl
+++ src/vagas.pl
@@ -1,4 +1,4 @@
-vaga(1,3,3,carro,1674331260,3-carro-3,abcabc).
+vaga(0,3,3,carro,1675435490,3-carro-3,none).
 vaga(1,3,1,moto,1674331310,3-moto-1,none).
 vaga(0,3,6,van,1674331354,3-van-6,none).
 vaga(0,7,3,carro,1674334264,3-carro-3,none).
